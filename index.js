ebbef927c26651551df5b607851134aa34fb21c7
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-05-07 07:43:37 +0600

    implement jwt functionality

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
 
 // middleware //
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5174'],
+  credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser())
 
 console.log(process.env.DB_PASS)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nshaxle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,15 +28,54 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares 
+const logger = async(req, res, next) => {
+  console.log('called these', req.host, req.originalUrl);
+
+  next();
+}
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token);
+   if(!token){
+    return res.status(401).send({message: 'not authorized'})
+   }
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized'})
+    }
+    req.user = decoded;
+    console.log('value of the token', decoded)
+    next()
+   })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    //-------------------AUTH RELATED API ------------------------//
+    app.post('/jwt', logger, async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie("token", token , {
+        httpOnly: true,
+        secure: false
+      })
+
+      .send({success: true})
+    })
+ 
+
+
+    //-------------------SERVICE RELATED API-------------------------//
     const servicesCollection = client.db('carDoctor').collection('services');
     const orderCollection = client.db('carDoctor').collection('order');
    //_______________All services load_______________________// 
-    app.get('/services', async(req, res) => {
+    app.get('/services', logger, async(req, res) => {
       const query = servicesCollection.find();
       const result = await query.toArray();
       res.send(result)
@@ -51,6 +96,39 @@ async function run() {
       const order = req.body;
       // console.log(order);
       const result = await orderCollection.insertOne(order);
+      res.send(result)
+    })
+    // _______get orders of specific email___________//
+    app.get('/order', logger, verifyToken,  async (req, res) => {
+      console.log('from valid token', req.user)
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await orderCollection.find(query).toArray();
+      res.send(result)
+    })
+    // ____________delete a order_____________________//
+    app.delete('/order/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await orderCollection.deleteOne(query);
+      res.send(result)
+    });
+    app.patch('/order/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const orderUpdate = req.body;
+      console.log(orderUpdate);
+      const updateDoc = {
+        $set: {
+          status: orderUpdate.status
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
     // Send a ping to confirm a successful connection
